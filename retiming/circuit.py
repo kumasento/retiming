27bfbce8c16345f9532c68ce1dcd06ad39be779d
Ruizhe <vincentzhaorz@gmail.com>
2019-04-16 15:10:56 +0100

    Finished the clock period computation

--- retiming/circuit.py
+++ retiming/circuit.py
@@ -11,38 +11,11 @@ def create_graph():
   return nx.MultiDiGraph()
 
 
-def get_registers(G):
-  """ Get all registers in G.
-  
-    Registers are labelled with an 'R' prefix.
-  """
-  assert isinstance(G, nx.MultiDiGraph)
-
-  return [n for n in G.nodes() if n.startswith(REG_PREFIX)]
-
-
 def get_vertexes(G):
   """ Get all vertexes in G. """
   assert isinstance(G, nx.MultiDiGraph)
 
   return [n for n in G.nodes() if n.startswith(VTX_PREFIX)]
-
-
-def add_register(G):
-  """ Add a register to a graph.
-  
-    Returns:
-      A str typed label.
-  """
-  assert isinstance(G, nx.MultiDiGraph)
-
-  regs = get_registers(G)
-  label = '{}{}'.format(REG_PREFIX, len(regs) + 1)
-
-  # simply add a new node
-  G.add_node(label)
-
-  return label
 
 
 def add_vertex(G, delay):
@@ -61,7 +34,7 @@ def add_vertex(G, delay):
 
   vtxs = get_vertexes(G)
   label = '{}{}'.format(VTX_PREFIX, len(vtxs) + 1)
-  G.add_node(label, delay=delay)
+  G.add_node(label, weight=delay)
 
   return label
 
@@ -70,32 +43,113 @@ def is_in_graph(G, v):
   """ Check whether a node is in the graph. """
   assert isinstance(v, str)  # v should be a label
 
-  if v.startswith(REG_PREFIX):
-    # TODO: cache this information in the graph
-    regs = get_registers(G)
-    return v in regs
-
-  if v.startswith(VTX_PREFIX):
-    # TODO: cache this information in the graph
-    vtxs = get_vertexes(G)
-    return v in vtxs
-
-  raise ValueError(
-      'v is not prefixed properly for a node in the circuit graph.')
+  return v in G.nodes()
 
 
-def add_edge(G, u, v):
+def add_edge(G, u, v, n_regs=0):
   """ Add an edge from u to v.
   
     u and v should already be in the graph.
+
+    Args:
+      G(nx.MultiDiGraph)
+      u(str): source
+      v(str): destination
+      n_regs(int): number of registers on the edge
+    Returns:
+      None
   """
   assert isinstance(G, nx.MultiDiGraph)
   assert isinstance(u, str)
   assert isinstance(v, str)
+  assert isinstance(n_regs, int) and n_regs >= 0
 
   if not is_in_graph(G, u):
     raise ValueError('u {} is not a node in G.'.format(u))
   if not is_in_graph(G, v):
     raise ValueError('v {} is not a node in G.'.format(v))
 
-  G.add_edge(u, v)
+  G.add_edge(u, v, weight=n_regs)
+
+
+def get_path_weight(G, path):
+  """ Get the total weight of all vertexes and edges connecting them. 
+  
+  Args:
+    G(nx.MultiDiGraph)
+    path(list): a list of node labels
+  Returns:
+    A sum of all weight
+  """
+  assert isinstance(G, nx.MultiDiGraph)
+
+  # convert to an edge path
+  edges = list(nx.utils.pairwise(path))
+
+  sum_of_weight = 0
+  for i, (u, v) in enumerate(edges):
+    # edge weights (multiple edges)
+    for d in G[u][v].values():
+      sum_of_weight += d['weight']
+
+  return sum_of_weight
+
+
+def get_path_delay(G, path):
+  """ Get the total amount of delay of vertexes on the path. """
+  assert isinstance(G, nx.MultiDiGraph)
+
+  return sum([G.nodes[n]['weight'] for n in path])
+
+
+def validate_graph(G):
+  """ Validate whether the graph satisfies conditions mentioned 
+    in the paper. """
+  assert isinstance(G, nx.MultiDiGraph)
+
+  # check nodes
+  for v, w in G.nodes.data('weight'):
+    if w < 0:
+      raise ValueError('Node "{}" has negative delay {}.'.format(v, w))
+
+  # check edges
+  for u, v, w in G.edges.data('weight'):
+    if w < 0:
+      raise ValueError(
+          'Number of registers between nodes "{}" and "{}" is negative: {}.'.
+          format(u, v, w))
+
+  # check cycles
+  for path in nx.simple_cycles(G):
+    if get_path_weight(G, path) == 0:
+      raise ValueError(
+          'There should be at least an edge in the cycle "{}" that has positive weight.'
+          .format(path))
+
+
+def compute_clock_period(G):
+  """ Implements Algorithm-CP in the paper. """
+
+  # create a subgraph with zero-weight edges
+  # TODO: re-factorise as a function
+  edges = [e for e in G.edges.data('weight') if e[2] == 0]
+  G0 = create_graph()  # the subgraph
+  G0.add_nodes_from(G.nodes(data=True))
+  G0.add_weighted_edges_from(edges)
+
+  # topological sort
+  vtxs = nx.topological_sort(G0)  # G0 doesn't contain cycles
+
+  # initialise the delta LUT
+  delta = {}
+  for v in vtxs:
+    in_edges = G0.in_edges(v)
+    if not in_edges:
+      delta[v] = G0.nodes[v]['weight']  # init
+    else:
+      delta[v] = 0  # min
+      for u, _ in in_edges:
+        delta[v] = max(delta[v], delta[u] + G0.nodes[v]['weight'])
+
+  # return the maximal value of all delta values
+  return max(delta.values())
--- retiming/circuit_test.py
+++ retiming/circuit_test.py
@@ -12,25 +12,12 @@ class CircuitTest(unittest.TestCase):
     G = ct.create_graph()
     self.assertIsInstance(G, nx.MultiDiGraph)
 
-  def test_add_register(self):
-    G = ct.create_graph()
-    R1 = ct.add_register(G)
-    R2 = ct.add_register(G)
-
-    self.assertEqual(R1, 'R1')
-    self.assertEqual(R2, 'R2')
-
-  def test_get_registers(self):
-    G = ct.create_graph()
-    R1 = ct.add_register(G)
-    R2 = ct.add_register(G)
-    self.assertListEqual(ct.get_registers(G), ['R1', 'R2'])
-
   def test_add_vertex(self):
     G = ct.create_graph()
 
     V1 = ct.add_vertex(G, 1)
     self.assertEqual(V1, 'V1')
+    self.assertEqual(G.node[V1]['weight'], 1)
 
     # delay should be nonnegative
     with self.assertRaises(AssertionError):
@@ -38,19 +25,97 @@ class CircuitTest(unittest.TestCase):
 
   def test_add_edge(self):
     G = ct.create_graph()
-    R = ct.add_register(G)
     V1 = ct.add_vertex(G, 2)
     V2 = ct.add_vertex(G, 2)
 
-    # edge from V1 to R, R to V2
-    ct.add_edge(G, V1, R)
-    ct.add_edge(G, R, V2)
+    # edge from V1 to V2
+    ct.add_edge(G, V1, V2, n_regs=1)
 
-    self.assertEqual(len(G.nodes()), 3)
-    self.assertEqual(len(G.edges()), 2)
+    self.assertEqual(len(G.nodes()), 2)
+    self.assertEqual(len(G.edges()), 1)
+    self.assertEqual(len(G['V1']['V2']), 1)
+    # access the weight
+    self.assertEqual(list(G['V1']['V2'].values())[0]['weight'], 1)
 
     with self.assertRaises(ValueError):
       ct.add_edge(G, 'p', 'q')
+
+  def test_get_path_weight(self):
+    G = ct.create_graph()
+    V1 = ct.add_vertex(G, 1)
+    V2 = ct.add_vertex(G, 2)
+    V3 = ct.add_vertex(G, 5)
+    V4 = ct.add_vertex(G, 8)
+
+    ct.add_edge(G, V1, V2, n_regs=3)
+    ct.add_edge(G, V2, V3, n_regs=2)
+    ct.add_edge(G, V3, V4, n_regs=1)
+
+    self.assertEqual(ct.get_path_weight(G, [V1, V2, V3, V4]), 6)
+
+  def test_get_path_delay(self):
+    G = ct.create_graph()
+    V1 = ct.add_vertex(G, 1)
+    V2 = ct.add_vertex(G, 2)
+    V3 = ct.add_vertex(G, 5)
+    V4 = ct.add_vertex(G, 8)
+
+    ct.add_edge(G, V1, V2, n_regs=3)
+    ct.add_edge(G, V2, V3, n_regs=2)
+    ct.add_edge(G, V3, V4, n_regs=1)
+
+    self.assertEqual(ct.get_path_delay(G, [V1, V2, V3, V4]), 16)
+
+  def test_validate_graph(self):
+    G = ct.create_graph()
+    V1 = ct.add_vertex(G, 1)
+    V2 = ct.add_vertex(G, 2)
+    V3 = ct.add_vertex(G, 5)
+    V4 = ct.add_vertex(G, 8)
+
+    ct.add_edge(G, V1, V2, n_regs=3)
+    ct.add_edge(G, V2, V3, n_regs=2)
+    ct.add_edge(G, V3, V4, n_regs=1)
+    ct.add_edge(G, V3, V1, n_regs=0)
+    ct.add_edge(G, V4, V1, n_regs=0)
+
+    ct.validate_graph(G)
+
+    # negate node weight
+    G.nodes[V1]['weight'] = -1
+    with self.assertRaises(ValueError):
+      ct.validate_graph(G)
+    G.nodes[V1]['weight'] = 0
+
+    # negate the edge weight
+    G[V1][V2][0]['weight'] = -1
+    with self.assertRaises(ValueError):
+      ct.validate_graph(G)
+
+    G[V1][V2][0]['weight'] = 1
+    ct.add_edge(G, V1, V1, n_regs=0)  # zero register self-cycle
+    with self.assertRaises(ValueError):
+      ct.validate_graph(G)
+
+  def test_compute_clock_period(self):
+    """ """
+    G = ct.create_graph()
+    V1 = ct.add_vertex(G, 1)
+    V2 = ct.add_vertex(G, 2)
+    V3 = ct.add_vertex(G, 3)
+    V4 = ct.add_vertex(G, 4)
+
+    ct.add_edge(G, V1, V2, n_regs=0)
+    ct.add_edge(G, V2, V3, n_regs=2)
+    ct.add_edge(G, V2, V4, n_regs=0)
+    ct.add_edge(G, V1, V4, n_regs=0)
+
+    cp = ct.compute_clock_period(G)
+    self.assertEqual(cp, 7)
+
+    G.nodes[V3]['weight'] = 9
+    cp = ct.compute_clock_period(G)
+    self.assertEqual(cp, 9)
 
 
 if __name__ == '__main__':
